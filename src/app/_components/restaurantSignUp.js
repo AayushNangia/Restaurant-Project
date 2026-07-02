da0f2981fdc0e1d5d9c216553b7c12807af4e36c
Jatin Gupta <jatin.gupta@melonleaf.com>
2026-07-02 06:56:33 -0400

    feat :

--- src/app/_components/restaurantSignUp.js
+++ src/app/_components/restaurantSignUp.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useRouter } from "next/navigation"
 const RestaurantSignUp =()=>{
     const[email,setEmail] =useState("");
     const[password,setPassword] =useState("");
